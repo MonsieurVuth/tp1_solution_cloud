5b56e14379545bef700d22e1d2eba8acb376d713
MonsieurVuth <daravuth.pith@epsi.fr>
2019-02-22 11:56:50 +0000

    modified

--- mysqlSaveBdd.sh
+++ mysqlSaveBdd.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 DATE=`date +%Y-%m-%d`
-mkdir -p saveBdd/$DATE
-cd saveBdd/$DATE
+mkdir saveBdd/$DATE
 
 mysqldump -P 3306 -u root -proot nextcloud | gzip > nextcloud.sql.gz
-scp -r -p ../$DATE root@192.168.33.201:/sauvegarde-bdd/completes/
+scp -r saveBdd/$DATE root@192.168.33.201:~/tp1_solution_cloud/sauvegarde-bdd/completes/
 
-rsync /var/log/mysql/mysql-bin.000002 root@192.168.33.201:/sauvegarde-bdd/incre/$DATE/
+rsync /var/log/mysql/mysql-bin.000002 root@192.168.33.201:~/tp1_solution_cloud/sauvegarde-bdd/incre/$DATE/
--- nextcloud-connect.sh
+++ nextcloud-connect.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+mkdir sauvegarde-bdd
+mkdir sauvegarde-bdd/completes
+mkdir sauvegarde-bdd/incre
+
 ssh root@192.168.33.200 'bash -s' <  mysqlSaveBdd.sh
 
